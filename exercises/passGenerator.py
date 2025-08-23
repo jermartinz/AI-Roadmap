0fbf8f0d46a294d5cdd9dfed033be7d2e24b3b11
Jeremy A. <61643086+jermartinz@users.noreply.github.com>
2025-08-23 02:35:15 -0400

    Update mini proyect: Add an option to generate other passwords and ask the user to enter a valid option if they make a mistake.

--- exercises/passGenerator.py
+++ exercises/passGenerator.py
@@ -4,7 +4,6 @@ import string
 
 print('This is a password generator')
 length = 0
-user_input = input('Do you want a (b)asic, (m)edium, or (s)trong password? ').strip().lower()
 all_characters = string.ascii_letters + string.digits + string.punctuation
 
 def randomizer_string(user_option,characters, user_length):
@@ -19,14 +18,15 @@ def randomizer_string(user_option,characters, user_length):
     password = ''.join(random.choices(characters, k=user_length))
     return password
 
-password = randomizer_string(user_input,all_characters,length)
+while True:
+    user_input = input('Do you want a (b)asic, (m)edium, or (s)trong password? ').strip().lower()
+    password = randomizer_string(user_input,all_characters,length)
 
-if password is None:
-    print("Please enter a valid option: b / m / s")
-else: print("Your password is: ", password)
-# Ask to user if want a password (basic, medium strong)
-#
-#
-# If basic password length(10) if medium password length 16,  
-# if strong password length 24
-# and return string of characters
+    if password is not None:
+        print("Your password is: ", password)
+        again = input("Do you want another password? (y/n): ").strip().lower()
+        if again != "y":
+            print("Goodbye!")
+            break
+    else:
+        print("Please enter a valid option: b / m / s") 
